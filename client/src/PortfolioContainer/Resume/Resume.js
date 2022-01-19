7ecce825e2518ef51259462da9703cdd49c71c7e
Anuksha Pasayat <anukshapasayat@gmail.com>
2022-01-20 02:25:02 +0530

    First commit

---
+++ client/src/PortfolioContainer/Resume/Resume.js
@@ -0,0 +1,240 @@
+import FadeIn from 'react-fade-in';
+import React, { useState, useEffect } from "react";
+import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
+import ScrollService from "../../utilities/ScrollService";
+import Animations from "../../utilities/Animations";
+import "./Resume.css";
+
+export default function Resume(props) {
+  const [selectedBulletIndex, setSelectedBulletIndex] = useState(0);
+  const [carousalOffsetStyle, setCarousalOffsetStyle] = useState({});
+  let fadeInScreenHandler = (screen) => {
+    if (screen.fadeInScreen !== props.id) return;
+    Animations.animations.fadeInScreen(props.id);
+  };
+  const fadeInSubscription =
+    ScrollService.currentScreenFadeIn.subscribe(fadeInScreenHandler);
+
+  const ResumeHeading = (props) => {
+    return (
+      <div className="resume-heading">
+        <div className="resume-main-heading">
+          <div className="heading-bullet"></div>
+          <span>{props.heading ? props.heading : ""}</span>
+          {props.fromDate && props.toDate ? (
+            <div className="heading-date">
+              {props.fromDate + "-" + props.toDate}
+            </div>
+          ) : (
+            <div></div>
+          )}
+        </div>
+        <div className="resume-sub-heading">
+          <span>{props.subHeading ? props.subHeading : ""}</span>
+        </div>
+        <div className="resume-heading-description">
+          <span>{props.description ? props.description : ""}</span>
+        </div>
+      </div>
+    );
+  };
+
+  const resumeBullets = [
+    { label: "Education", logoSrc: "education.svg" },
+    { label: "Work History", logoSrc: "work-history.svg" },
+    { label: "Programming Skills", logoSrc: "programming-skills.svg" },
+    { label: "Projects", logoSrc: "projects.svg" },
+    { label: "Interests", logoSrc: "interests.svg" },
+  ];
+
+  const programmingSkillsDetails = [
+    { skill: "JavaScript", ratingPercentage: 45 },
+    { skill: "React JS", ratingPercentage: 50 },
+    { skill: "C", ratingPercentage: 85 },
+    { skill: "Java", ratingPercentage: 89 },
+    { skill: "Node JS", ratingPercentage: 50 },
+    { skill: "C++", ratingPercentage: 70 },
+    { skill: "Python", ratingPercentage: 50 },
+    { skill: "HTML", ratingPercentage: 80 },
+    { skill: "CSS", ratingPercentage: 80 },
+    { skill: "R", ratingPercentage: 50 },
+  ];
+
+  const projectsDetails = [
+    {
+      title: "Personal Portfolio Website",
+      duration: { fromDate: "2020", toDate: "2021" },
+      description:
+        "A Personal Portfolio website to showcase all my details and projects at one place.",
+      subHeading: "Technologies Used: React JS, Bootsrap",
+    },
+    {
+      title: "Movie recommendation System",
+      duration: { fromDate: "2020", toDate: "2021" },
+      description:
+        "A machine learning model to recommend Movies.",
+      subHeading: "Technologies Used: Python, NumPy, Pandas, sklearn",
+    },
+    {
+      title: "Salary Prediction Model-Using Linear Regression",
+      duration: { fromDate: "2020", toDate: "2021" },
+      description:
+      "A machine learning model to predict salary using Linear Regression Algorithm.",
+      subHeading: "Technologies Used: Python, NumPy, Pandas, sklearn",
+    },
+  ];
+
+  const resumeDetails = [
+    <div className="resume-screen-container" key="education">
+      <ResumeHeading
+        heading={"Kalinga Institute of Industrial Technology, Bhubhaneswar."}
+        subHeading={"BACHELOR OF TECHNOLOGY IN INFORMATION TECHNOLOGY"}
+        fromDate={"2019"}
+        toDate={"2023"}
+      />
+
+      <ResumeHeading
+        heading={"Loyola School, Jamshedpur"}
+        subHeading={"Intermediate"}
+        fromDate={"2017"}
+        toDate={"2019"}
+      />
+      <ResumeHeading
+        heading={"Loyola School, Jamshedpur"}
+        subHeading={"Matriculate"}
+        fromDate={"2006"}
+        toDate={"2017"}
+      />
+    </div>,
+
+    /* WORK EXPERIENCE */
+    <div className="resume-screen-container" key="work-experience">
+      <div className="experience-container">
+        <ResumeHeading
+          heading={"Portfolio"}
+          subHeading={"FULL STACK DEVELOPER"}
+          fromDate={"Jan"}
+          toDate={"Feb"}
+        />
+        <div className="experience-description">
+          <span className="resume-description-text">
+            I created a portfolio website using React library.
+          </span>
+        </div>
+        <div className="experience-description">
+          <br />
+        </div>
+      </div>
+    </div>,
+
+    /* PROGRAMMING SKILLS */
+    <div
+      className="resume-screen-container programming-skills-container"
+      key="programming-skills"
+    >
+      {programmingSkillsDetails.map((skill, index) => (
+        <div className="skill-parent" key={index}>
+          <div className="heading-bullet"></div>
+          <span>{skill.skill}</span>
+          <div className="skill-percentage">
+            <div
+              style={{ width: skill.ratingPercentage + "%" }}
+              className="active-percentage-bar"
+            ></div>
+          </div>
+        </div>
+      ))}
+    </div>,
+
+    /* PROJECTS */
+    <div className="resume-screen-container" key="projects">
+      {projectsDetails.map((projectsDetails, index) => (
+        <ResumeHeading
+          key={index}
+          heading={projectsDetails.title}
+          subHeading={projectsDetails.subHeading}
+          description={projectsDetails.description}
+          fromDate={projectsDetails.duration.fromDate}
+          toDate={projectsDetails.duration.toDate}
+        />
+      ))}
+    </div>,
+
+    /* Interests */
+    <div className="resume-screen-container" key="interests">
+      <ResumeHeading
+        heading="Painting"
+        description="Apart from being a tech enthusiast and a code writer, i also love to paint."
+      />
+      <ResumeHeading
+        heading="Music"
+        description="Listening to soothing music is something i can never compromise with, skimming through Spotify's pop songs charts is at times the best stress reliever that i can get my hands on."
+      />
+      <ResumeHeading
+        heading="Reading"
+        description="I like to reading drama novels, it easily grabs my attention and it's something I can do for hours."
+      />
+    </div>,
+  ];
+
+  const handleCarousal = (index) => {
+    let offsetHeight = 360;
+
+    let newCarousalOffset = {
+      style: { transform: "translateY(" + index * offsetHeight * -1 + "px)" },
+    };
+
+    setCarousalOffsetStyle(newCarousalOffset);
+    setSelectedBulletIndex(index);
+  };
+
+  const getBullets = () => {
+    return resumeBullets.map((bullet, index) => (
+      <div
+        onClick={() => handleCarousal(index)}
+        className={
+          index === selectedBulletIndex ? "bullet selected-bullet" : "bullet"
+        }
+        key={index}
+      >
+        <img
+          className="bullet-logo"
+          src={require(`../../assets/Resume/${bullet.logoSrc}`)}
+          alt="B"
+        />
+        <span className="bullet-label">{bullet.label}</span>
+      </div>
+    ));
+  };
+
+  const getResumeScreens = () => {
+    return (
+      <div
+        style={carousalOffsetStyle.style}
+        className="resume-details-carousal"
+      >
+        {resumeDetails.map((ResumeDetail) => ResumeDetail)}
+      </div>
+    );
+  };
+  
+  return (
+    <FadeIn>
+      <div className="resume-container screen-container" id={props.id || ""}>
+      <div className="resume-content">
+        <ScreenHeading title={"Resume"} subHeading={"My formal Bio Details"} /><div className="resume-card">
+          <div className="resume-bullets">
+            <div className="bullet-container">
+              <div className="bullet-icons"></div>
+              <div className="bullets">{getBullets()}</div>
+            </div>
+          </div>
+
+          <div className="resume-bullet-details">{getResumeScreens()}</div>
+        </div>
+      </div>
+    </div>
+    </FadeIn>
+    
+  );
+}
